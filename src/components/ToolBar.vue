64253fd80d1dab3b135c8aecc3c81e3c32069980
YoonPic <hyosang.yoon2@gmail.com>
2021-06-17 17:14:37 +0900

    first commit

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='header'>
+      <router-link to='/news'>News</router-link> |
+      <router-link to='/ask'>ask</router-link> |
+      <router-link to='/jobs'>jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+    color: white;
+    background: #42b883;
+    display: flex;
+    padding: 8px;
+}
+.header .router-link-exact-active {
+    color : yellow;
+}
+.header a {
+    color: white;
+}
+</style>
